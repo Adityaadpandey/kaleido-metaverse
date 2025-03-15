96b8506402ef7c79c22b931a1b9e30c5faaf1ce3
Aditya <Adityapandeyadp@gmail.com>
2025-03-15 20:51:49 +0530

    add NextAuth integration with GitHub provider and update authentication flow

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,27 @@
-const page = () => {
-  return <div>page</div>;
-};
 
-export default page;
+import { auth, signIn } from "@/lib/auth"
+
+export default async function SignIn() {
+  const session = await auth()
+
+  if (!session) {
+    return (
+      <form
+        action={async () => {
+          "use server"
+          await signIn("github")
+        }}
+      >
+        <button type="submit">Signin with GitHub</button>
+      </form>
+    )
+  }
+  console.log(session)
+  return (
+    <div>
+      <p>Already signed in</p>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+    </div>
+  )
+
+}
